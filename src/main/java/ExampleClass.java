8bf71b64d231610fc4f20766dac568b12dd21c14
Chris Kiriakou <chris-kiriakou@web.de>
2023-04-27 16:00:10 +0200

    Initail commit.

---
+++ src/main/java/ExampleClass.java
@@ -0,0 +1,8 @@
+public class ExampleClass {
+    public void exampleMethod() {
+    int a = 5;
+    int b = 10;
+    int sum = a + b;
+    System.out.println("The sum of " + a + " and " + b + " is " + sum);}
+}
+
